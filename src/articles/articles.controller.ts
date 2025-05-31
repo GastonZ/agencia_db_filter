8c7cc0fabdd2641b43c70483384b2bb983c36577
Gaston <gastonzappullaryu@gmail.com>
2025-05-31 02:26:24 -0300

    agencia filter db

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Controller('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+  
+  @Get()
+  findAll() {
+    return this.articlesService.findAll();
+  }
+}
---
+++ src/articles/articles.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.article.findMany({ where: { published: true}}) ;
+  }
+}
