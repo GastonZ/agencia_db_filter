17729f5f87c28c9a512b0ea5bafaaed101eb8dde
Gaston <gastonzappullaryu@gmail.com>
2025-06-02 10:06:54 -0300

    -prisma module

--- src/exc_files/exc_files.module.ts
+++ src/exc_files/exc_files.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExcFilesService } from './exc_files.service';
 import { ExcFilesController } from './exc_files.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ExcFilesController],
   providers: [ExcFilesService],
+  imports: [PrismaModule]
 })
-export class ExcFilesModule {}
+export class ExcFilesModule { }
