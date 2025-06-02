f41afee5536d312b93c77755ad5213010515175f
Gaston <gastonzappullaryu@gmail.com>
2025-06-02 12:56:09 -0300

    cors added

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Agencia back filter')
     .setDescription('Agencia back filter backend')
